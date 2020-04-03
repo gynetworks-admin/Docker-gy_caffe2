08f673ede00e36b325c22e10ae8bf8b5db38510a
gynetworks-admin <31232595+gynetworks-admin@users.noreply.github.com>
2020-04-03 14:28:57 +0900

    Update Docker_run.sh

--- Docker_run.sh
+++ Docker_run.sh
@@ -3,7 +3,6 @@ sudo docker run \
         -it \
         --rm \
         --shm-size 4G \
-        --name="gy_caffe2" \
         --volume $(pwd):$(pwd) \
         --workdir="$(pwd)" \
         gynetworks/gy_caffe2:c2v0.8.1.cuda8.cudnn7.ubuntu16.04
